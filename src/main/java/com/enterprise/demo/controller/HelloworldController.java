ec88b3e37bf74ff5c957eec8c8f31d72ade33d9a
88Chang88 <chang1050833599@hotmail.com>
2015-08-24 23:51:21 +0800

    SpringMVC框架搭建框架

---
+++ src/main/java/com/enterprise/demo/controller/HelloworldController.java
@@ -0,0 +1,17 @@
+package com.enterprise.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/helloworld")
+public class HelloworldController {
+	
+	@RequestMapping(value="/index",method=RequestMethod.GET)
+	public String index(){
+		System.out.println("20150824");
+		return "index";
+	}
+
+}
